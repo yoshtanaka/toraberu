820ce66ead4377e6b482066c886fc1bff9902e7d
yoshtanaka <yoshi128349@gmail.com>
2018-06-17 18:56:48 +0900

    initialization

---
+++ travelSpring/src/main/java/comt/travel/springboot/TravelSpringApplication.java
@@ -0,0 +1,12 @@
+package comt.travel.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelSpringApplication.class, args);
+	}
+}
